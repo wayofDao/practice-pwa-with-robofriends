bca785b339ad98425f5726d94d8ea554f60f971b
Giedrius Dev <giedriusthedev@gmail.com>
2018-09-16 19:59:27 +0200

    improving accessibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id, username }) => {
 	
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img  alt='robo' src={`https://robohash.org/${id}?200x200`} />
+			<img  alt='robo' src={`https://robohash.org/${id}?size=200x200`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
